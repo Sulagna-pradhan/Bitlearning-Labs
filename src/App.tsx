ea5f5ccb0395d57d7be3cc9b9d40434c7ed05ae8
Sulagna Pradhan <infinityresources81@gmail.com>
2025-10-26 21:54:54 +0530

    Changes to be committed:

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,6 @@ import WhyUs from './pages/WhyUs';
 import Testimonial from './pages/Testimonial';
 import OurStory from './pages/OurStory';
 import Career from './pages/Career';
-//Service Pages
-import Wedding from './pages/services/Wedding';
-import Corporate from './pages/services/Corporate';
-import PrivateParties from './pages/services/PrivateParties';
-import Birthday from './pages/services/Birthday';
-import Holiday from './pages/services/Holiday';
 //404 Not Found Page
 import NotFound from './pages/NotFound';
 
@@ -38,12 +32,6 @@ const App: React.FC = () => {
           <Route path="/testimonials" element={<Testimonial />} />
           <Route path="/story" element={<OurStory />} />
           <Route path="/careers" element={<Career />} />
-
-          <Route path="/service/wedding-catering" element={<Wedding />} />
-          <Route path="/service/corporate-events" element={<Corporate />} />
-          <Route path="/service/private-parties" element={<PrivateParties />} />
-          <Route path="/service/birthday-celebrations" element={<Birthday />} />
-          <Route path="/service/holiday-catering" element={<Holiday />} />
 
           <Route path="*" element={<NotFound />} />
         </Routes>
