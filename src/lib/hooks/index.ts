aa13d6c2717c77fc81ec9ff6f970608d480a301d
Toufique Al Ajay <87595397+karnerfly@users.noreply.github.com>
2025-10-29 09:36:48 +0530

    Add useDebounce hook and integrate in Home page
    Introduced a new useDebounce hook for debouncing input values. Updated Home.tsx to use useDebounce for the name input before storing it in localStorage, improving performance and user experience. Also updated hooks index to export useDebounce.

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -2,5 +2,6 @@ import useIsMobile from './use-mobile';
 import useLocalStorage from './use-localstorage';
 // import useQueryFilter from './use-queryfilter';
 import useTheme from './use-theme';
+import useDebounce from './use-debounce';
 
-export { useIsMobile, useLocalStorage, useTheme };
+export { useIsMobile, useLocalStorage, useTheme, useDebounce };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import { useIsMobile, useLocalStorage, useTheme } from '../lib/hooks';
+import React, { useEffect, useState } from 'react';
+import {
+  useDebounce,
+  useIsMobile,
+  useLocalStorage,
+  useTheme,
+} from '../lib/hooks';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
 const Homepage: React.FC = () => {
-  const [name, setName] = useLocalStorage('userName', '');
+  const [name, setName] = useState<string>('');
+  const debouncedName = useDebounce(name);
+  const [storeName, setNameToStore] = useLocalStorage('userName', '');
   const isMobile = useIsMobile();
   const { theme, toggleTheme } = useTheme();
+
+  useEffect(() => {
+    setNameToStore(debouncedName);
+  }, [debouncedName]);
 
   const fetchTodoById = async (id: string) => {
     try {
@@ -20,7 +31,7 @@ const Homepage: React.FC = () => {
     }
   };
 
-  const { data, error, isFetching } = useSuspenseQuery({
+  const { error, isFetching } = useSuspenseQuery({
     queryKey: ['testing'],
     queryFn: () => fetchTodoById('1'),
   });
@@ -29,14 +40,12 @@ const Homepage: React.FC = () => {
     return <h1>Error while fetching data!!</h1>;
   }
 
-  console.log(data);
-
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#f5f2e8] to-[#e7e3c2] dark:from-[#2a2818] dark:to-[#45422a] overflow-x-hidden flex items-center justify-center">
       <div className="container bg-amber-50 dark:bg-amber-400 w-fit p-4">
         <h2 className="text-3xl font-bold">Home Page</h2>
         <p>IsMobile: {isMobile ? 'true' : 'false'}</p>
-        <p>Name: {name}</p>
+        <p>Name: {storeName}</p>
         <input
           type="text"
           placeholder="Enter your name"
